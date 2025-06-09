157eec39200407e6ac4c26916cf300e23b1256f4
annumallesh28 <rashmibm3579@gmail.com>
2025-06-09 06:42:09 +0530

    save the file

---
+++ Calculate.java
@@ -0,0 +1,24 @@
+public class Calculate {
+    
+    public static int calculateSum(int[] scores) {
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return sum;
+    }
+
+    public static double calculateAverage(int[] scores) {
+        int sum = calculateSum(scores);
+        return (double) sum / scores.length;
+    }
+
+    public static void main(String[] args) {
+        int[] scores = {85, 90, 78, 92, 88};
+        int sum = calculateSum(scores);
+        double average = calculateAverage(scores);
+        
+        System.out.println("Sum of scores: " + sum);
+        System.out.println("Average score: " + average);
+    }
+}
